3fbee8a4ce78a1fea21552e9e7ecdbdda53c644a
Billel T <148473122+BillelT@users.noreply.github.com>
2025-04-03 21:28:15 +0200

    all file on MEI format have note classed and colored differently

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
-// const test = document.querySelectorAll("g.note")
+document.addEventListener("DOMContentLoaded", (event) => {
+    verovio.module.onRuntimeInitialized = () => {
+        let tk = new verovio.toolkit();
+        fetch("./src/score.mei")
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (meiXML) {
+                let svg = tk.renderData(meiXML, {});
+                document.getElementById("notation").innerHTML = svg;
+                // console.log(tk.getElementAttr(pname));
 
+                const notes = document.querySelectorAll('.note');
 
-// window.addEventListener('DOMContentLoaded', () => {
-//     const notes = document.getElementsByClassName("note")
+                Array.from(notes).forEach(note => {
+                    const pname = tk.getElementAttr(note.id).pname
 
-//     console.log(notes)
-// })
+                    switch (pname) {
+                        case 'a':
+                            note.classList.add('note-a');
+                            break;
+                        case 'b':
+                            note.classList.add('note-b');
+                            break;
+                        case 'c':
+                            note.classList.add('note-c');
+                            break;
+                        case 'd':
+                            note.classList.add('note-d');
+                            break;
+                        case 'e':
+                            note.classList.add('note-e');
+                            break;
+                        case 'f':
+                            note.classList.add('note-f');
+                            break;
+                        case 'g':
+                            note.classList.add('note-g');
+                            break;
+                        default:
+                            note.classList.add('note-unknown');
+                            break;
+                    }
+                })
+            }
+            )
+    };
+});
+
