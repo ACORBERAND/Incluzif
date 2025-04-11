596a54a7ab4cf0368d19cb36c02b2cc8fde8d815
Billel T <148473122+BillelT@users.noreply.github.com>
2025-04-11 10:25:47 +0200

    mapping on scores collections on index and page generated according to the content of the collections socre

--- .eleventy.js
+++ .eleventy.js
@@ -4,14 +4,15 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("script.js");
-  eleventyConfig.addPassthroughCopy("styles"); 
+  eleventyConfig.addPassthroughCopy("styles");
 
   return {
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data", 
-      output: "_site"
+      input: "content",
+      includes: "../_includes",
+      data: "_data",
+      output: "_site",
+      content: "content"
     }
   };
 };
---
+++ content/score/score2/score2.md
@@ -0,0 +1,8 @@
+---
+layout: "layouts/playing.html"
+title: "score2"
+tags: "scores"
+author: "author2"
+style: "pop"
+mei_file: "score2.mei"
+---
